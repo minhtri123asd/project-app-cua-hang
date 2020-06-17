3964af180475970f74386ac42b26c1237dfdf425
minhtri123asd <58369080+minhtri123asd@users.noreply.github.com>
2020-06-17 11:13:11 +0700

    fix react navigation

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import * as React from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './screens/Loign '
+import Login from './screens/Login'
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+   headerShown: false
+ }}>
         <Stack.Screen name="Home" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
